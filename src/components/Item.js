9ced5c7b181f2cc31d7b6badd0c02b4c2d2301b0
Maurice Smith <Xmaurice.smith@gmail.com>
2021-11-04 14:12:24 -0400

    commit tree b41749afa92e9f2e1415eabe090e525b3f8069cd

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,70 @@
-import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [isIncart, setIsInCart] = useState(false)
+
+  function handleInCart() {
+    setIsInCart(!isIncart)
+  }
+  
+const appClass = isIncart ? 'in-cart' : ''
+
+const cartLabel = isIncart ? 'Remove from Cart' : 'Add to Cart'
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleInCart} className="add">{cartLabel}</button>
     </li>
   );
 }
 
 export default Item;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {useState }from "react";
+
+// function Item({ name, category }) {
+//   const [isIncart, setIsInCart] = useState(false)
+
+//   function handleClick() {
+// setIsInCart(!isIncart)
+//   }
+
+//   const appClass = isIncart ? 'in-cart' : ''
+
+//   return (
+//     <li className={appClass}>
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button onClick={handleClick} className="add">{isIncart ? 'Remove from Cart' : 'Add to Cart'}</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
